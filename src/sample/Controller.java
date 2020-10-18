aa700471b411f9f084213b8a1031e06f7e552fa1
Mannix <mannix_chen@163.com>
2020-10-18 11:47:38 +0800

    准备初始化表格前的备份

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,27 +3,41 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ComboBox;
 import javafx.event.ActionEvent;
 import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
 
 public class Controller {
 
     @FXML
-    private Button mButton;
+    private Button onButton;
     @FXML
-    private Label mLabel;
-    @FXML
-    private ComboBox comboBox_API;
+    private TableView<String> openFilesTable;
 
-    Integer ClickCount=0;
+    ReadDiskFile rd;
 
 
-    private ObservableList<String> apiList=FXCollections.observableArrayList();
+    private ObservableList<String> openFilesList=FXCollections.observableArrayList();
     public void Init(){
-
+        rd =  new ReadDiskFile();
+        initTable();//初始化表格
     }
 
+    private void initTable() {
+        for (String[] curFiles:
+             rd.openFilesInfo) {
+            for (String curInfo:
+                 curFiles) {
+                openFilesList.add(curInfo);
+            }
+        }
+        openFilesTable.setItems(openFilesList);
+        System.out.println(openFilesList);
+    }
+
+    @FXML//事件监听器
+    public void onButtonClick(ActionEvent event){
+        System.out.println(77777);
+    }
 
 }
---
+++ src/sample/ReadDiskFile.java
@@ -0,0 +1,31 @@
+package sample;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+public class ReadDiskFile {
+    /*这个类用来读取diskfile，用于将diskFile的文件渲染到前端*/
+
+
+
+
+    /*以下为未与后台整合的版本的成员以及构造方法*/
+    String openFilesInfo[][];
+
+    ReadDiskFile(){
+        openFilesInfo = new String[5][5];
+        for (int i = 0 ; i < 1 ; i++ )
+        openFilesInfo[i]= new String[]{"java.txt", "F:\\study\\JavaStudy\\OSKS\\src\\sample", "3", "4", "只读"};
+
+    }
+    /*以下为与后台整合后的版本的成员以及构造方法*/
+
+    ReadDiskFile(File diskFile){
+
+    }
+
+
+
+
+
+}
